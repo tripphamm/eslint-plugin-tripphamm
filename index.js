4ca2a7e963bb51247fa116a86d0572663b616e90
EJ Hammond <ejhammond@hey.com>
2020-01-05 14:42:46 -0500

    New: Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nodeConfig = require('./configs/node');
+const reactConfig = require('./configs/react');
+
+module.exports = {
+  configs: {
+    node: nodeConfig,
+    react: reactConfig,
+  },
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@tripphamm/trippkit/configs/lint-staged');
