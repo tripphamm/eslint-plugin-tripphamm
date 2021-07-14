fe9afe6aabf094fd6750d4261a6a7c838f48361b
EJ Hammond <ejhammond@hey.com>
2021-07-14 12:49:44 -0400

    Fix: Export base config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const baseConfig = require('./configs/base');
 const nodeConfig = require('./configs/node');
 const reactConfig = require('./configs/react');
 
 module.exports = {
   configs: {
+    base: baseConfig,
     node: nodeConfig,
     react: reactConfig,
   },
